3d2373050563b504b45ee7f2fd79a93aeb964cc0
td10031t <75354050+td10031t@users.noreply.github.com>
2020-12-02 14:06:11 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://td10031t.github.io/frog1002/model.json';
 
 
 function preload() {
